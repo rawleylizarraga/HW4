2eb0dca8435cb5c39e7b01bc5a760dfb37d4e287
Rawley Lizarraga <rawleylizarraga@gmail.com>
2026-03-30 16:44:00 -0700

    list working and gets

--- index.mjs
+++ index.mjs
@@ -1,10 +1,31 @@
 import express from 'express';
+import { getAll, searchByName } from 'rest-countries';
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-   res.send('Hello Express app!')
+app.get('/', async (req, res) => {
+   const countries = await getAll(['name', 'flags', 'cca3']);
+   countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+   console.log(countries);
+   res.render("index", { countries })
+});
+
+app.get('/country', async (req, res) => {
+   // let countryCode = req.query.code;
+   // let countryInfo = countries[`get${countryCode}`]();
+   // res.render('planet', { countryInfo, countryName });
+
+   let countryCode = req.query.code;
+
+   let url = `https://restcountries.com/v3.1/alpha/${countryCode}?fields=name,flags,area,capital,population`; // max 10 fields
+   let response = await fetch(url);
+   let countryInfo = await response.json();
+
+   console.log(url);
+   console.log(countryInfo);
+
+   res.render('country', { countryInfo, countryCode });
 });
 
 app.listen(3000, () => {
---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+// event listners
+
+
+// variables
+
+
+// functions
+// populateList();
+
+// function populateList() {
+//     for (let i of "<%= countryInfo %>") {
+//         document.querySelector("#countryList").innerHTML += `<li> <%= countryInfo.name.common %> </li>`;
+//     }
+// }
